68bb2f1f6876b0eeb584aeec74b367d87d4835bc
Linnea Larsson <philar-9@student.ltu.se>
2026-04-21 18:58:57 +0200

    feat(db): add initial entities
    More testing to ensue...

---
+++ src/main/java/se/ltu/eduo/model/UserPreference.java
@@ -0,0 +1,28 @@
+package se.ltu.eduo.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user_preferences", schema = "eduo")
+public class UserPreference {
+    @Id
+    @Column(name = "user_id", nullable = false)
+    private Integer id;
+
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "locale", length = Integer.MAX_VALUE)
+    private String locale;
+
+
+}
