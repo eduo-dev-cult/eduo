46cf5720073927d94712bd15074b37d24fd094ae
Linnea Larsson <philar-9@student.ltu.se>
2026-04-22 15:36:40 +0200

    feat(db): add repository classes for user management and spring injection

---
+++ src/main/java/se/ltu/eduo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package se.ltu.eduo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import se.ltu.eduo.model.User;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByid(Integer id);
+}
