378055b3af1b51d427a3b9a6c7258cb40a99f07d
MartinTCode <mtul@protonmail.com>
2026-05-04 23:27:46 +0200

    Interface for the services was never created. So mock ought to have called error before as well. Fixed.

--- src/main/java/se/ltu/eduo/service/LlmService.java
+++ src/main/java/se/ltu/eduo/service/LlmService.java
@@ -1,11 +1,5 @@
 package se.ltu.eduo.service;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
-// interface för interaktion med ai, används för både mock och riktig ai implementation
-@Service
-@Profile("ollama")
 public interface LlmService {
     String generateStudyQuestions(String prompt);
 }
