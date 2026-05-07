626967c1a73bf7fd00815a43ce8a91488b6883fc
Linnea Larsson <philar-9@student.ltu.se>
2026-05-07 19:01:49 +0200

    feat: update GenerationDto with new settings

--- src/main/java/se/ltu/eduo/dto/GenerationDto.java
+++ src/main/java/se/ltu/eduo/dto/GenerationDto.java
@@ -1,18 +1,36 @@
 package se.ltu.eduo.dto;
 
 import lombok.Value;
+import se.ltu.eduo.model.collection.Generation;
+import se.ltu.eduo.model.collection.GenerationFocusArea;
+import se.ltu.eduo.model.collection.GenerationLanguage;
 
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.UUID;
 
 /**
- * DTO for {@link se.ltu.eduo.model.collection.Generation}
+ * DTO for {@link Generation}
  */
 @Value
 public class GenerationDto implements Serializable {
     UUID id;
+    CollectionDto collection;
     Instant createdAt;
     Instant updatedAt;
     QuizDto quiz;
+    int numOfQuestions;
+    GenerationLanguage language;
+    GenerationFocusArea focusArea;
+    String topics;
+    boolean easy;
+    boolean medium;
+    boolean hard;
+    boolean multipleChoice;
+    boolean openEnded;
+    boolean trueFalse;
+    boolean questions;
+    boolean correctAnswers;
+    boolean explanations;
+    boolean description;
 }
