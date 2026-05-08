d90b516ec4343d7838c2cd2c6523de3252977f84
Linnea Larsson <philar-9@student.ltu.se>
2026-05-08 13:11:47 +0200

    refactor: turned request into record, removed uuid field as that is a pathvariable

--- src/main/java/se/ltu/eduo/dto/request/CreateGenerationRequest.java
+++ src/main/java/se/ltu/eduo/dto/request/CreateGenerationRequest.java
@@ -1,31 +1,34 @@
 package se.ltu.eduo.dto.request;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import se.ltu.eduo.model.collection.GenerationFocusArea;
 import se.ltu.eduo.model.collection.GenerationLanguage;
 import java.io.Serializable;
 import java.util.UUID;
 
-public class CreateGenerationRequest implements Serializable {
-    //relations
-    UUID collection; //id of collection it belongs to
-    UUID[] sourceMaterials;
-    //endregion relations
+public record CreateGenerationRequest(
+    // relations
+    //UUID collection, unneeded, exists in pathvar
+    @NotEmpty
+    UUID[] sourceMaterials,
+    // endregion relations
 
-    //region settings
-    int numOfQuestions;
-    GenerationLanguage language;
-    GenerationFocusArea focusArea;
-    String topics;
-    boolean easy;
-    boolean medium;
-    boolean hard;
-    boolean multipleChoice;
-    boolean openEnded;
-    boolean trueFalse;
-    boolean questions;
-    boolean correctAnswers;
-    boolean explanations;
-    boolean description;
-    //endregion
-}
+    // region settings
+    int numOfQuestions,
+    GenerationLanguage language,
+    GenerationFocusArea focusArea,
+    String topics,
+    boolean easy,
+    boolean medium,
+    boolean hard,
+    boolean multipleChoice,
+    boolean openEnded,
+    boolean trueFalse,
+    boolean questions,
+    boolean correctAnswers,
+    boolean explanations,
+    boolean description
+    // endregion
+) {}
