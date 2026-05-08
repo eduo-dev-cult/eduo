aeff2db183b6dd6f021a78d3cdb74242170f733a
Linnea Larsson <philar-9@student.ltu.se>
2026-05-08 12:01:39 +0200

    feat: update creategenerationrequest with new settings fields

--- src/main/java/se/ltu/eduo/dto/request/CreateGenerationRequest.java
+++ src/main/java/se/ltu/eduo/dto/request/CreateGenerationRequest.java
@@ -1,11 +1,31 @@
 package se.ltu.eduo.dto.request;
 
-import java.util.List;
+
+import se.ltu.eduo.model.collection.GenerationFocusArea;
+import se.ltu.eduo.model.collection.GenerationLanguage;
+import java.io.Serializable;
 import java.util.UUID;
 
-public record CreateGenerationRequest(List<UUID> sourceMaterialIds) {
-    //lista av source materials
-    //alla inställningar som ska användas
-    //vilken collection det hör till (oh därmed användare)
-    //
+public class CreateGenerationRequest implements Serializable {
+    //relations
+    UUID collection; //id of collection it belongs to
+    UUID[] sourceMaterials;
+    //endregion relations
+
+    //region settings
+    int numOfQuestions;
+    GenerationLanguage language;
+    GenerationFocusArea focusArea;
+    String topics;
+    boolean easy;
+    boolean medium;
+    boolean hard;
+    boolean multipleChoice;
+    boolean openEnded;
+    boolean trueFalse;
+    boolean questions;
+    boolean correctAnswers;
+    boolean explanations;
+    boolean description;
+    //endregion
 }
