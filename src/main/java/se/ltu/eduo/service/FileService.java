f6182769b477387d3ed9f68268a3223372736ebf
vehul <venhul-4@student.ltu.se>
2026-05-11 12:15:59 +0200

    refactor:  fileService now uses sourceMaterialRepository instead of collectionsService.

--- src/main/java/se/ltu/eduo/service/FileService.java
+++ src/main/java/se/ltu/eduo/service/FileService.java
@@ -1,24 +1,29 @@
 package se.ltu.eduo.service;
 
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import se.ltu.eduo.repository.SourceMaterialRepository;
 
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
-
+@RequiredArgsConstructor
 @Service
 public class FileService {
 
-    private final CollectionService collectionService;
-
-    public FileService(CollectionService collectionService) {
-        this.collectionService = collectionService;
-    }
+    private final SourceMaterialRepository sourceMaterialRepository;
 
     public String getFileAsString(UUID sourceMaterialId) {
-        byte[] fileData = collectionService.getSourceMaterial(sourceMaterialId).getFileData();
+
+        byte[] fileData = sourceMaterialRepository.findById(sourceMaterialId)
+                .orElseThrow(() ->
+                        new EntityNotFoundException("Source material not found: " + sourceMaterialId))
+                .getFileData();
+
         return new String(fileData, StandardCharsets.UTF_8);
     }
 }
+
 
 
