1ab897a21f40d70295fbb68d2cf551dc2b810088
Linnea Larsson <philar-9@student.ltu.se>
2026-05-11 09:05:05 +0200

    test: add profile to contextload due to profile usage causing breakage

--- src/test/java/se/ltu/eduo/EduoApplicationTests.java
+++ src/test/java/se/ltu/eduo/EduoApplicationTests.java
@@ -2,10 +2,12 @@ package se.ltu.eduo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 @ContextConfiguration(initializers = TestContainersInitializer.class)
+@ActiveProfiles("test")
 class EduoApplicationTests {
 
     @Test
