82ef521c334e9578314c0aa1e8d4655b895eedc3
Linnea Larsson <philar-9@student.ltu.se>
2026-05-18 18:31:34 +0200

    feat: add user preference crud repository interface

---
+++ src/main/java/se/ltu/eduo/user/repository/UserPreferencesRepository.java
@@ -0,0 +1,7 @@
+package se.ltu.eduo.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import se.ltu.eduo.user.model.UserPreferences;
+
+public interface UserPreferencesRepository extends JpaRepository<UserPreferences, Integer> {
+}
